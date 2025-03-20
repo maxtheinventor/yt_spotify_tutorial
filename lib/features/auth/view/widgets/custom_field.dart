09399081b99dc7b5970839343d725a01fafc6604
Max <maxtheinventor@protonmail.com>
2025-03-20 13:56:21 +0100

    updates

--- lib/features/auth/view/widgets/custom_field.dart
+++ lib/features/auth/view/widgets/custom_field.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
   final String hintText;
+  final TextEditingController controller;
 
-  const CustomField({super.key, required this.hintText});
+  const CustomField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(decoration: InputDecoration(hintText: hintText));
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(hintText: hintText),
+    );
   }
 }
