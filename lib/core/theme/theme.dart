9dd086dd8d47d4cba4dc3ea021153929afb1107d
Max <maxtheinventor@protonmail.com>
2025-03-20 10:46:47 +0100

    Initial commit

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:yt_spotify_tutorial/core/theme/app_pallete.dart';
+
+class AppTheme {
+
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor
+  );
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:yt_spotify_tutorial/core/theme/theme.dart';
+import 'package:yt_spotify_tutorial/features/auth/view/pages/signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme.darkThemeMode,
+      home: const SignupPage(),
+    );
+  }
+}
